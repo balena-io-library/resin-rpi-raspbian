e409e210a440897e515e0a84a9f3336be5e52b01
James <james@resin.io>
2014-11-24 11:44:03 +0000

    Change to UNIX script file

--- updater.sh
+++ updater.sh
@@ -5,6 +5,6 @@ docker run -d -i -t --name img-updater resin/rpi-raspbian:wheezy /bin/bash
 docker exec img-updater apt-get update
 docker exec img-updater apt-get upgrade -y
  # Generate version tag for docker image: resin/rpi-raspbian:wheezy-YYYY-MM-DD
- # Default OS_NAME=resin/rpi-raspbian:wheezy
-VERSIONTAG="resin/rpi-raspbian:wheezy-$(date +'%F')"
+OS_NAME="resin/rpi-raspbian:wheezy" 
+VERSIONTAG="$OS_NAME-$(date +'%F')"
 docker export img-updater | docker import - $VERSIONTAG

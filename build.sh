f5d8ea595d165c9ad57165aa3ed4f76e84813c87
Trong Nghia Nguyen <nghiant2710@gmail.com>
2017-06-12 23:35:18 +0700

    Update latest base images fixes

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ dir=$(mktemp --tmpdir=/var/tmp -d)
 date=$(date +'%Y%m%d' -u)
 	
 mkdir -p $dir/rootfs/usr/bin
-cp qemu-arm-static $dir/rootfs/usr/bin
+cp qemu-arm-static resin-xbuild $dir/rootfs/usr/bin
 chmod +x $dir/rootfs/usr/bin/qemu-arm-static
 	
 ./mkimage.sh -t $REPO:$SUITE --dir=$dir debootstrap --foreign --variant=minbase --keyring=/root/.gnupg/pubring.gpg --arch=armhf --include=sudo,ca-certificates,dirmngr $SUITE $MIRROR

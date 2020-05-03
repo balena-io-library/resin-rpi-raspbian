e5f829e208a5ac819efbba551a9663435cb9b897
Trong Nghia Nguyen <nghiant2710@gmail.com>
2020-05-03 10:35:21 +0700

    Fix QEMU url
    Change-type: patch
    Signed-off-by: Trong Nghia Nguyen <nghiant2710@gmail.com>

--- automation/build-image.sh
+++ automation/build-image.sh
@@ -3,14 +3,14 @@
 set -o errexit
 set -o pipefail
 
-QEMU_VERSION='4.0.0-balena2-arm'
+QEMU_VERSION='4.0.0.balena2-arm'
 QEMU_SHA256='ae0144b8b803ddb8620b7e6d5fd68e699a97e0e9c523d283ad54fcabc0e615f8'
 RESIN_XBUILD_VERSION='1.0.0'
 RESIN_XBUILD_SHA256='1eb099bc3176ed078aa93bd5852dbab9219738d16434c87fc9af499368423437'
 MIRROR='http://archive.raspbian.org/raspbian'
 
 # Download QEMU
-curl -SLO https://github.com/balena-io/qemu/releases/download/v4.0.0-balena/qemu-$QEMU_VERSION.tar.gz \
+curl -SLO https://github.com/balena-io/qemu/releases/download/v4.0.0+balena2/qemu-$QEMU_VERSION.tar.gz \
 	&& echo "$QEMU_SHA256  qemu-$QEMU_VERSION.tar.gz" > qemu-$QEMU_VERSION.tar.gz.sha256sum \
 	&& sha256sum -c qemu-$QEMU_VERSION.tar.gz.sha256sum \
 	&& tar -xz --strip-components=1 -f qemu-$QEMU_VERSION.tar.gz

eaf5da21f81b523f35831ca429f0dcb311856a81
Kyle Harding <kyle@balena.io>
2021-02-09 09:01:58 -0500

    Update QEMU to v5.2.0+balena4
    Change-type: patch
    Connected-to: https://github.com/balena-io/qemu/issues/21
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- automation/build-image.sh
+++ automation/build-image.sh
@@ -3,17 +3,18 @@
 set -o errexit
 set -o pipefail
 
-QEMU_VERSION='5.2.0.balena1-arm'
-QEMU_SHA256='2eb115f1b1626765108da1265b56444ffbfce86f194611d4367e1a06508857f8'
+QEMU_RELEASE='5.2.0+balena4'
+QEMU_ASSET="qemu-${QEMU_RELEASE/+/.}-arm.tar.gz"
+QEMU_SHA256='8b410eabfb8417b6d8dcb7008fafa91c5f0496ab5db0b93a1b5b88cc7d86391e'
 RESIN_XBUILD_VERSION='1.0.0'
 RESIN_XBUILD_SHA256='1eb099bc3176ed078aa93bd5852dbab9219738d16434c87fc9af499368423437'
 MIRROR='http://archive.raspbian.org/raspbian'
 
 # Download QEMU
-curl -SLO https://github.com/balena-io/qemu/releases/download/v5.2.0+balena1/qemu-v$QEMU_VERSION.tar.gz \
-	&& echo "$QEMU_SHA256  qemu-v$QEMU_VERSION.tar.gz" > qemu-v$QEMU_VERSION.tar.gz.sha256sum \
-	&& sha256sum -c qemu-v$QEMU_VERSION.tar.gz.sha256sum \
-	&& tar -xz --strip-components=1 -f qemu-v$QEMU_VERSION.tar.gz
+curl -SLO https://github.com/balena-io/qemu/releases/download/v$QEMU_RELEASE/$QEMU_ASSET \
+	&& echo "$QEMU_SHA256 $QEMU_ASSET" > $QEMU_ASSET.sha256sum \
+	&& sha256sum -c $QEMU_ASSET.sha256sum \
+	&& tar -xz --strip-components=1 -f $QEMU_ASSET
 curl -SLO http://resin-packages.s3.amazonaws.com/resin-xbuild/v$RESIN_XBUILD_VERSION/resin-xbuild$RESIN_XBUILD_VERSION.tar.gz \
 	&& echo "$RESIN_XBUILD_SHA256  resin-xbuild$RESIN_XBUILD_VERSION.tar.gz" | sha256sum -c - \
 	&& tar -xzf resin-xbuild$RESIN_XBUILD_VERSION.tar.gz
